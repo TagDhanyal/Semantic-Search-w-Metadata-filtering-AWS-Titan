8e026ba7f3e16b0be8a1169d62ab40ccb4948170
TagDhanyal <dhanyaltag716@gmail.com>
2023-10-29 20:50:38 +0200

    v1

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# Semantic-Search-w-Metadata-filtering-AWS-Titan
- 
+# Workshop Demo: Semantic Search with Amazon Titan
+
+## Introduction
+
+I recently had the opportunity to participate in a workshop on implementing semantic search using Amazon Titan. The workshop was a comprehensive exploration of various technologies and tools, and it enabled me to gain a deep understanding and practical experience with implementing advanced features.
+
+## Workshop Overview
+
+The workshop focused on creating a semantic search system using Amazon Titan's capabilities for document retrieval and language modeling. Here are the key components and steps involved:
+
+1. **Data Retrieval**
+
+   The first step was to retrieve PDF documents from URLs using the `urlretrieve` function. The documents used in the workshop were annual shareholder letters from Amazon spanning several years. Each document was associated with metadata, such as the year and source.
+
+2. **Data Preprocessing**
+
+   To prepare the PDF documents for analysis, we used the `PdfReader` and `PdfWriter` from the `pypdf` library to extract the text content and reduce the number of pages in each document. This preprocessing step aimed to create cleaner, more manageable data for subsequent analysis.
+
+3. **Document Splitting**
+
+   Next, we learned about document splitting techniques. We used a Recursive Character Text Splitter to segment the text content into smaller, more manageable chunks. This step is essential for creating embeddings and conducting efficient searches.
+
+4. **Building a Vector Database**
+
+   We then utilized Amazon Titan's capabilities to create a vector database. This database was constructed using document embeddings generated from the segmented documents. Amazon Titan's powerful embeddings allowed for efficient and effective search.
+
+5. **Semantic Search**
+
+   We learned how to perform semantic searches using the created vector database. The workshop covered various search techniques, including basic similarity search, similarity search with metadata filtering, top-K matching, and maximal marginal relevance. These search methods provided different ways to retrieve relevant documents based on the query.
+
+6. **Language Model Integration**
+
+   We also integrated Amazon Titan's language model into the search process. The model was used to generate responses to specific queries, enhancing the search results by providing more context and information.
+
+7. **Chain Creation**
+
+   Finally, we created a RetrievalQA chain to automate the search and language model integration process. The chain allowed for querying the vector database and generating responses based on a given context and question.
+
+## Workshop Outcome
+
+Through this workshop, I gained a deep understanding of semantic search systems, document retrieval, document preprocessing, vector databases, and language model integration. I also learned how to create and manage complex chains to automate the search process.
+
+This hands-on experience has equipped me with the knowledge and skills required to implement similar systems in real-world applications. I am now confident in using Amazon Titan's capabilities to create powerful semantic search solutions, benefiting from its document retrieval and language modeling features.
---
+++ titan_semantic_search.py
@@ -0,0 +1,213 @@
+# Import necessary libraries
+from urllib.request import urlretrieve
+import glob
+from pypdf import PdfReader, PdfWriter
+import numpy as np
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader
+import os
+
+# Define the URLs of the PDF documents to retrieve
+urls = [
+    'https://s2.q4cdn.com/299287126/files/doc_financials/2023/ar/2022-Shareholder-Letter.pdf',
+    'https://s2.q4cdn.com/299287126/files/doc_financials/2022/ar/2021-Shareholder-Letter.pdf',
+    'https://s2.q4cdn.com/299287126/files/doc_financials/2021/ar/Amazon-2020-Shareholder-Letter-and-1997-Shareholder-Letter.pdf',
+    'https://s2.q4cdn.com/299287126/files/doc_financials/2020/ar/2019-Shareholder-Letter.pdf'
+]
+
+# Define the filenames for the retrieved PDF documents
+filenames = [
+    'AMZN-2022-Shareholder-Letter.pdf',
+    'AMZN-2021-Shareholder-Letter.pdf',
+    'AMZN-2020-Shareholder-Letter.pdf',
+    'AMZN-2019-Shareholder-Letter.pdf'
+]
+
+# Define the metadata for each PDF document
+metadata = [
+    dict(year=2022, source=filenames[0]),
+    dict(year=2021, source=filenames[1]),
+    dict(year=2020, source=filenames[2]),
+    dict(year=2019, source=filenames[3])]
+
+# Define the data root directory where the retrieved PDF documents will be saved
+# Define the absolute path to the 'data' directory
+data_root = os.path.abspath("data")
+
+# Check if the 'data' directory exists, and create it if it doesn't
+if not os.path.exists(data_root):
+    os.makedirs(data_root)
+
+# Retrieve the PDF documents from the URLs and save them to the data root directory
+for idx, url in enumerate(urls):
+    file_path = os.path.join(data_root, filenames[idx])  # Use os.path.join to create an absolute path
+    urlretrieve(url, file_path)
+
+# Preprocess the PDF documents by removing the first three pages and saving the remaining pages to a new PDF file
+local_pdfs = glob.glob(data_root + '*.pdf')
+
+for local_pdf in local_pdfs:
+    pdf_reader = PdfReader(local_pdf)
+    pdf_writer = PdfWriter()
+
+    # Remove the first three pages of the PDF document
+    for pagenum in range(len(pdf_reader.pages) - 3):
+        page = pdf_reader.pages[pagenum]
+        pdf_writer.add_page(page)
+
+    # Save the preprocessed PDF document to the same filename
+    with open(local_pdf, 'wb') as new_file:
+        new_file.seek(0)
+        pdf_writer.write(new_file)
+        new_file.truncate()
+
+# Split the PDF documents into smaller chunks of text
+text_splitter = RecursiveCharacterTextSplitter(
+    # Set a really small chunk size, just to show.
+    chunk_size=1000,
+    chunk_overlap=100,
+)
+
+documents = []
+
+docs = text_splitter.split_documents(documents)
+
+# Import necessary libraries
+import os
+import boto3
+import json
+import sys
+
+module_path = ".."
+sys.path.append(os.path.abspath(module_path))
+from utils import bedrock
+
+bedrock_client = bedrock.get_bedrock_client(
+    assumed_role=os.environ.get("BEDROCK_ASSUME_ROLE", None),
+    region=os.environ.get("AWS_DEFAULT_REGION", None),
+    runtime=True
+)
+
+from utils.TokenCounterHandler import TokenCounterHandler
+from langchain.embeddings import BedrockEmbeddings
+from langchain.schema import Document
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
+from langchain.llms.bedrock import Bedrock
+
+# Initialize Token Counter Handler
+token_counter = TokenCounterHandler()
+
+# Initialize BedrockEmbeddings
+embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1", client=bedrock_client)
+
+# Process documents and prepare them to be converted to vectors
+docs = []
+
+# Define your document list here
+
+db = FAISS.from_documents(docs, embeddings)
+
+# Save and load the vector store from the local filesystem
+db.save_local("faiss_titan_index")
+new_db = FAISS.load_local("faiss_titan_index", embeddings)
+db = new_db
+
+# Set your search query
+query = "How has AWS evolved?"
+
+# Basic Similarity Search
+results_with_scores = db.similarity_search_with_score(query)
+for doc, score in results_with_scores:
+    print(f"Content: {doc.page_content}\nMetadata: {doc.metadata}\nScore: {score}\n\n")
+
+# Similarity Search with Metadata Filtering
+filter = dict(year=2022)
+results_with_scores = db.similarity_search_with_score(query, filter=filter)
+for doc, score in results_with_scores:
+    print(f"Content: {doc.page_content}\nMetadata: {doc.metadata}, Score: {score}\n\n")
+
+# Top-K Matching
+k = 2
+fetch_k = 4
+results = db.similarity_search(query, filter=filter, k=k, fetch_k=fetch_k)
+for doc in results:
+    print(f"Content: {doc.page_content}\nMetadata: {doc.metadata}\n\n")
+
+# Maximal Marginal Relevance
+results = db.max_marginal_relevance_search(query, filter=filter)
+for doc in results:
+    print(f"Content: {doc.page_content}\nMetadata: {doc.metadata}\n\n")
+
+# Set up LangChain LLM with specified parameters
+model_kwargs_titan = {
+    "maxTokenCount": 512,
+    "stopSequences": [],
+    "temperature": 0.0,
+    "topP": 0.5
+}
+
+# Initialize LLM
+llm = Bedrock(
+    model_id="amazon.titan-text-express-v1",
+    client=bedrock_client,
+    model_kwargs=model_kwargs_titan,
+    callbacks=[token_counter]
+)
+
+# Test LLM with a prompt
+prompt = "How has AWS evolved?"
+result = llm.predict(prompt)
+print(result)
+token_counter.report()
+
+# Define a prompt template
+prompt_template = """
+Human: Here is a set of context, contained in <context> tags:
+
+<context>
+{context}
+</context>
+
+Use the context to provide an answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+{question}
+
+Assistant:"""
+
+PROMPT = PromptTemplate(
+    template=prompt_template,
+    input_variables=["context", "question"]
+)
+
+# Initialize RetrievalQA chain
+qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=db.as_retriever(
+        search_type="similarity",
+        search_kwargs={"k": 5, "filter": filter},
+        callbacks=[token_counter]
+    ),
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": PROMPT
+},
+    callbacks=[token_counter]
+)
+
+# Test the RetrievalQA chain with queries
+queries = [
+    "How has AWS evolved?",
+    "Why is Amazon successful?",
+    "What business challenges has Amazon experienced?",
+    "How was Amazon impacted by COVID-19?",
+]
+
+for query in queries:
+    result = qa({"query": query})
+    print(f'Query: {result["query"]}\n')
+    print(f'Result: {result["result"]}\n')
+    print(f'Context Documents: ')
+    for srcdoc in result["source_documents"]:
+        print(f'{srcdoc}\n')
